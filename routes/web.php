4f90c0c28717b13c1d7eeeae52ebd999ad670b8f
John Michael Monteza <johnmichaelmonteza@gmail.com>
2024-09-08 12:26:44 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+ 
+Route::get('/students/create', function () {
+    $student = new  Student();
+    $student ->first_name = 'Decemae';
+    $student ->last_name = 'Robles';
+    $student ->email = 'decemaerobles69@gmail.com';
+    $student ->save();
+    return 'Student Created!';
 });
+
+Route::get('/students', function () {
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email', 'decemaerobles69@gmail.com')->first();
+    $student->email = 'decemaerobles69@gmail.com';
+    $student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/students/delete', function () {
+    $student = Student::where('email', 'decemaerobles69@gmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
+
+Route::get('/courses/create', function () {
+    $course = new Course();
+    $course->course_name = 'Introduction to Database';
+    $course->save();
+    return 'Course Created'; 
+});
+?>
